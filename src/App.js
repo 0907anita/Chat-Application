62ebc4d24330ba4cb68753ce5bed2d4ebcf6cef8
Anita <09anita07choudhary@gmail.com>
2022-03-25 10:23:24 +0530

    Chat-Application using Reactj.s

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = 'bfbca186-5c8a-4742-ab9c-92f023f05590';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
